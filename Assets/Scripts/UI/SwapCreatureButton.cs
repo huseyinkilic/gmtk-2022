1209990ce8a64332b81092a2c1180a76e17f625e
FreezeDriedMangoes <claytonharting+github@gmail.com>
2022-07-16 14:31:01 -0400

    created swap buttons and hooked them up to the turn manager

---
+++ Assets/Scripts/UI/SwapCreatureButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapCreatureButton : MonoBehaviour
+{
+    public int team;
+    public int creatureIndex;
+    public void MakeAndSubmitAction()
+    {
+        var action = TurnManager.Instance.MakeSwitchAction(team, creatureIndex);
+        TurnManager.Instance.SubmitAction(action);
+    }
+}
