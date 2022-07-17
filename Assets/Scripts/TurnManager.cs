985ce148796c9af3befaa5ec230638cd0633a11c
FreezeDriedMangoes <claytonharting+github@gmail.com>
2022-07-17 14:21:32 -0400

    stuff

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -89,6 +89,58 @@ public class TurnManager : MonoBehaviour, ITurnManager
         for (int i = 0; i < 2; i++) players.Add(null);
     }
 
+
+    public GameObject builderMenu;
+    public GameObject battleMenu;
+    public void HANDLE_START()
+    {
+        if (TeamBuilderButton.includedCreatures.Count < 3) return;
+
+        var creatures = Database.Instance.GetAllCreatures();
+        CreatureState[] team = new CreatureState[3];
+        for (int j = 0; j < team.Length; j++)
+        {
+            int c = Random.Range(0, creatures.Count-1);
+            team[j] = new CreatureState()
+            {
+                definition = creatures[j], // just use the last 3 creatures listed in the database (test creatures)
+                knownMoves = new Move[]
+                {
+                    Database.Instance.MoveFromID(creatures[j].allowedMoves[0]), // just use the first 4 allowable moves
+                    Database.Instance.MoveFromID(creatures[j].allowedMoves[1]),
+                    Database.Instance.MoveFromID(creatures[j].allowedMoves[2]),
+                    Database.Instance.MoveFromID(creatures[j].allowedMoves[3])
+                }
+            };
+        }
+
+        TurnManager.Instance.InitializePlayer(1, team);
+
+    
+        CreatureState[] team1 = new CreatureState[3];
+        Creature[] creatures1 = TeamBuilderButton.includedCreatures.ToArray();
+        for (int j = 0; j < team1.Length; j++)
+        {
+            team1[j] = new CreatureState()
+            {
+                definition = creatures1[j], // just use the last 3 creatures listed in the database (test creatures)
+                knownMoves = new Move[]
+                {
+                    Database.Instance.MoveFromID(creatures1[j].allowedMoves[0]), // just use the first 4 allowable moves
+                    Database.Instance.MoveFromID(creatures1[j].allowedMoves[1]),
+                    Database.Instance.MoveFromID(creatures1[j].allowedMoves[2]),
+                    Database.Instance.MoveFromID(creatures1[j].allowedMoves[3])
+                }
+            };
+        }
+
+        builderMenu.SetActive(false);
+        battleMenu.SetActive(true);
+    
+        BattleUI.Instance.player2AI = new SimpleAI();
+        TurnManager.Instance.InitializePlayer(0, team);
+    }
+
     //
     // Interface functions
     // 
