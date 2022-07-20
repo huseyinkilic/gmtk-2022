33fa87427c24384068ea1ce026ae1add939ea2d0
FreezeDriedMangoes <claytonharting+github@gmail.com>
2022-07-19 23:30:45 -0400

    fix: resolved stack overflow game crash when the player won, forced the action log to stay scrolled at the bottom if the scroll bar is already at the bottom

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,14 @@ public class PlayerController
 
     public void ForceSwitch() 
     { 
+        // determine if the game is over
+        var player1Lost = TurnManager.Instance.players[0].team.All(c => !c.CanStillFight());
+        var player2Lost = TurnManager.Instance.players[1].team.All(c => !c.CanStillFight());
+        if (player1Lost && player2Lost) { IUI.Instance.GameOver(-1);                    return; }
+        else if (player1Lost)           { IUI.Instance.GameOver(ITurnManager.PLAYER_2); return; }
+        else if (player2Lost)           { IUI.Instance.GameOver(ITurnManager.PLAYER_1); return; }
+
+
         pendingForcedSwitch = true;
         IUI.Instance.ForceSwitch(this.teamNumber); 
     }
--- Assets/Scripts/UI/ActionLogUI.cs
+++ Assets/Scripts/UI/ActionLogUI.cs
@@ -8,25 +8,45 @@ public class ActionLogUI : MonoBehaviour
     public static ActionLogUI Instance;
 
     public GameObject textPrefab;
+    public Transform parentTo;
 
     private float currentY = -30;
 
-    public Transform parentTo;
+    private ScrollRect scrollRect;
 
     // Start is called before the first frame update
     void Awake()
     {
         //parentTo = this.transform.GetChild(0);   
         Instance = this;
+        scrollRect = GetComponent<ScrollRect>();
     }
 
     public void AddLog(string msg)
     {
+        bool isAtBottom = scrollRect.normalizedPosition.y <= 0.00001; // basically "is equal to 0"
+        Debug.LogError($"vert norm {scrollRect.verticalNormalizedPosition}");
+
         GameObject logText = Instantiate(textPrefab);
         logText.GetComponent<Text>().text = msg;
         logText.transform.parent = parentTo;
         var deltaY = -30 - 155.8726f; // 155.8726 is the height of the log text
         logText.transform.localPosition = new Vector3(30, currentY+deltaY);
         currentY = currentY+deltaY;
+
+        if (isAtBottom)
+        {
+            // https://stackoverflow.com/a/47613689/9643841
+            Debug.LogError("scrolling to bottom");
+            Canvas.ForceUpdateCanvases();
+
+            parentTo.GetComponent<VerticalLayoutGroup>().CalculateLayoutInputVertical() ;
+            parentTo.GetComponent<ContentSizeFitter>().SetLayoutVertical() ;
+
+            scrollRect.content.GetComponent<VerticalLayoutGroup>().CalculateLayoutInputVertical() ;
+            scrollRect.content.GetComponent<ContentSizeFitter>().SetLayoutVertical() ;
+
+            scrollRect.verticalNormalizedPosition = 0;
+        }
     }
 }
